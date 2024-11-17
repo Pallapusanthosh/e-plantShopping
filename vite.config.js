fe88b3dcb108c90ca118d0c5a9bf2b4f32a3c55e
santhosh <psivsanthosh16@gmail.com>
2024-11-17 18:57:40 +0530

    deploy 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/shoppingreact",
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
-})
+  base: '/', // Adjust this to the correct base path if needed
+});
